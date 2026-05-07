40bbe0839c6e9b34070ce9462724c9a0ab8ad3de
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2026-05-07 17:23:38 +0300

    Use Map::iter() for image map iteration

--- src/karm-print/pdf-printer.cpp
+++ src/karm-print/pdf-printer.cpp
@@ -147,7 +147,7 @@ export struct PdfPrinter : FilePrinter {
 
             // Add all images to page resources
             Pdf::Dict pageImagesDict;
-            for (auto& [imageId, objRef] : imageId2ObjRef) {
+            for (auto& [imageId, objRef] : imageId2ObjRef.iter()) {
 
                 auto formattedName = Io::format("Im{}", imageId);
                 pageImagesDict.put(formattedName.str(), objRef);
